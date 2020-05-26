f83d12043749752bc31c345c94a12e6213722836
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-26 13:49:44 +0200

    Añadida función para recoger valores de TextField y funcionalidad del botón Añadir

--- src/ejercicio/FrameEjercicio3.java
+++ src/ejercicio/FrameEjercicio3.java
@@ -10,14 +10,35 @@ import javax.swing.*;
 import javax.swing.event.MouseInputListener;
 import javax.swing.filechooser.*;
 
+public class FrameEjercicio3 extends JFrame implements ActionListener, ItemListener {
 
-public class FrameEjercicio3 extends JFrame implements ActionListener, ItemListener{
+    /**
+     * Identifica los diferentes valores que hay en un TextField y los guarda en una colección
+     * 
+     * @param txfTextField TextField del que se recogerán los datos
+     * 
+     * @return Colección que los datos del TextField
+     */
+    public ArrayList<String> recogerValoresTXF(JTextField txfTextField) {
+        ArrayList<String> valoresTXF = new ArrayList<>();
+
+        for (int i = 0; i < txfTextField.getText().split(";").length; i++) {
+            if(!txfTextField.getText().split(";")[i].trim().equals("")){
+                valoresTXF.add(txfTextField.getText().split(";")[i]);
+            }
+        }
+
+        return valoresTXF;
+    }
+
     JComboBox cbA, cbB;
     JButton btnAnadir, btnQuitar, btnTraspasarA, btnTraspasarB;
     JTextField txf1, txf2;
     JLabel lblCantidadElementos, lblIndiceSeleccionado, lbl3;
 
-    public FrameEjercicio3(){
+    ArrayList<String> valoresTextField = new ArrayList<>(); // Creo una colección donde se guardarán los diferentes valores que contendrá el TextField indicado
+
+    public FrameEjercicio3() {
         super("Ejercicio 3");
         setLayout(new FlowLayout());
 
@@ -61,16 +82,35 @@ public class FrameEjercicio3 extends JFrame implements ActionListener, ItemListe
         lblIndiceSeleccionado = new JLabel();
         add(lblIndiceSeleccionado);
 
-
-        addWindowListener(new WindowAdapter() { // Aquí programo la confirmación al salir de esta ventana usando el adaptador de WindowListener
+        addWindowListener(new WindowAdapter() { // Aquí programo la confirmación al salir de esta ventana usando el
+                                                // adaptador de WindowListener
             public void windowClosing(WindowEvent e) {
-                int res = JOptionPane.showConfirmDialog(null, "Estás segur@?", "Cerrar?",
-                        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                int res = JOptionPane.showConfirmDialog(null, "Estás segur@?", "Cerrar?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 
                 if (res == JOptionPane.OK_OPTION) {
                     e.getWindow().dispose();
                 }
             }
         });
-    }   
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == btnAnadir){ // Acciones a realizar cuando se pulsa el botón para añadir valores al 1er TextField
+            if(recogerValoresTXF(txf1).size() == 0){
+                int res = JOptionPane.showMessageDialog(null, "No hay valores válidos en el TextField!", "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+            else{
+                valoresTextField = recogerValoresTXF(txf1);
+                for (int i = 0; i < valoresTextField.size(); i++) {
+                    cbA.addItem(valoresTextField.get(i));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+
+    }
 }

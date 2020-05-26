55ccbbc52e69e0268a978361f6757f5c519a8a44
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-26 17:25:32 +0200

    Añadidas funciones del botón Quitar

--- src/ejercicio/FrameEjercicio3.java
+++ src/ejercicio/FrameEjercicio3.java
@@ -13,7 +13,8 @@ import javax.swing.filechooser.*;
 public class FrameEjercicio3 extends JFrame implements ActionListener, ItemListener {
 
     /**
-     * Identifica los diferentes valores que hay en un TextField y los guarda en una colección
+     * Identifica los diferentes valores que hay en un TextField y los guarda en una
+     * colección
      * 
      * @param txfTextField TextField del que se recogerán los datos
      * 
@@ -23,7 +24,7 @@ public class FrameEjercicio3 extends JFrame implements ActionListener, ItemListe
         ArrayList<String> valoresTXF = new ArrayList<>();
 
         for (int i = 0; i < txfTextField.getText().split(";").length; i++) {
-            if(!txfTextField.getText().split(";")[i].trim().equals("")){
+            if (!txfTextField.getText().split(";")[i].trim().equals("")) {
                 valoresTXF.add(txfTextField.getText().split(";")[i]);
             }
         }
@@ -36,11 +37,32 @@ public class FrameEjercicio3 extends JFrame implements ActionListener, ItemListe
     JTextField txf1, txf2;
     JLabel lblCantidadElementos, lblIndiceSeleccionado, lbl3;
 
-    ArrayList<String> valoresTextField = new ArrayList<>(); // Creo una colección donde se guardarán los diferentes valores que contendrá el TextField indicado
+    ArrayList<String> valoresTextField = new ArrayList<>(); // Creo una colección donde se guardarán los diferentes
+                                                            // valores que contendrá el TextField indicado
 
     public FrameEjercicio3() {
         super("Ejercicio 3");
         setLayout(new FlowLayout());
+
+        class MouseHandler extends MouseAdapter { // Creo el MouseHandler para gestionar los eventos de ratón
+
+            @Override
+            public void mouseEntered(MouseEvent e) { 
+                if (e.getSource() == btnQuitar) { // Acciones a realizar cuando el cursor del ratón pasa por encima del botón de quitar
+                    btnQuitar.setForeground(Color.RED);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                if(e.getSource() == btnQuitar){ // Acciones a realizar cuando el cursor del ratón sale de encima del botón de quitar
+                    btnQuitar.setForeground(null);
+                }
+            }
+
+        }
+
+        MouseHandler mh = new MouseHandler();
 
         cbA = new JComboBox<String>();
         cbA.addItemListener(this);
@@ -58,6 +80,7 @@ public class FrameEjercicio3 extends JFrame implements ActionListener, ItemListe
         btnQuitar = new JButton("Quitar");
         btnQuitar.setSize(btnQuitar.getPreferredSize());
         btnQuitar.addActionListener(this);
+        btnQuitar.addMouseListener(mh);
         add(btnQuitar);
 
         btnTraspasarA = new JButton("Traspasar al primero");
@@ -82,8 +105,7 @@ public class FrameEjercicio3 extends JFrame implements ActionListener, ItemListe
         lblIndiceSeleccionado = new JLabel();
         add(lblIndiceSeleccionado);
 
-        addWindowListener(new WindowAdapter() { // Aquí programo la confirmación al salir de esta ventana usando el
-                                                // adaptador de WindowListener
+        addWindowListener(new WindowAdapter() { // Aquí programo la confirmación al salir de esta ventana usando el adaptador de WindowListener
             public void windowClosing(WindowEvent e) {
                 int res = JOptionPane.showConfirmDialog(null, "Estás segur@?", "Cerrar?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 
@@ -98,12 +120,35 @@ public class FrameEjercicio3 extends JFrame implements ActionListener, ItemListe
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == btnAnadir){ // Acciones a realizar cuando se pulsa el botón para añadir valores al 1er TextField
             if(recogerValoresTXF(txf1).size() == 0){
-                int res = JOptionPane.showMessageDialog(null, "No hay valores válidos en el TextField!", "ERROR", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "No hay valores válidos en el TextField!", "ERROR", JOptionPane.ERROR_MESSAGE);
             }
             else{
                 valoresTextField = recogerValoresTXF(txf1);
                 for (int i = 0; i < valoresTextField.size(); i++) {
                     cbA.addItem(valoresTextField.get(i));
+                }
+            }
+        }
+        else if(e.getSource() == btnQuitar){ // Acciones a realizar cuando se pulsa el botón de quitar elemento de cbA
+            if(txf2.getText() == ""){ // Acciones a realizar si no hay nada escrito en el segundo TextField
+                if(cbA.getSelectedItem() != null){ // Si hay un elemento seleccionado, se elimina del ComboBox
+                    cbA.removeItem(cbA.getSelectedItem());
+                }
+                else{ // Si no hay ningún elemento seleccionado, salta un mensaje de error
+                    JOptionPane.showMessageDialog(null, "No has seleccionado ningún elemento en el ComboBox!", "ERROR", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+            else{ // Acciones a realizar si hay algo escrito en el segundo TextField
+                int elementosDelCB = cbA.getItemCount();
+                for (int i = 0; i < cbA.getItemCount(); i++) {
+                    cbA.setSelectedIndex(i);
+                    if(cbA.getSelectedItem().toString().startsWith(txf2.getText())){ // Si hay algún elemento del combobox que empiece por el texto dentro de txf2
+                        cbA.removeItem(cbA.getSelectedItem()); // Se eliminará del combobox
+                    }
+                }
+
+                if(elementosDelCB == cbA.getItemCount()){ // Si no se ha quitado ningún elemento lo muestro con un JOptionPane
+                    JOptionPane.showMessageDialog(null, "No hay ningún elemento del ComboBox que empiece por " + txf2.getText(), "No se ha quitado nada", JOptionPane.INFORMATION_MESSAGE);
                 }
             }
         }
